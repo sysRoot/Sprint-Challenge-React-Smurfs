c65092f1b5a78eb4bf94754c6158881be93b8d97
Jason Cook <the.root.user@gmail.com>
2019-06-16 21:39:06 -0400

    Import dependencies, sample nav from Material-UI

--- village/src/App.js
+++ village/src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import NavBar from './components/NavBar'
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
@@ -11,12 +13,27 @@ class App extends Component {
       smurfs: [],
     };
   }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:3333/smurfs`)
+      .then(res => this.setState({ smurfs: res.data }))
+      .catch(err =>
+        console.log(
+          err,
+          `cDMount-time error, axios may have performed incorrectly`
+        )
+      );
+  }
+
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
+        <NavBar />
         <SmurfForm />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
